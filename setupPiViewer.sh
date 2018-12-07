4d4cb11fa33a894139a4537bd7ab2a3af5cd8a1b
Nattaphol <rootncdev2015@gmail.com>
2018-12-07 23:27:06 +0700

    Add dir checking

--- setupPiViewer.sh
+++ setupPiViewer.sh
@@ -2,8 +2,12 @@
 
 # usage: ./setupPiViewer.sh
 
-printf "Pulling git\n"
+printf "Pulling git...\n"
 git pull
-printf "Moving PiViewer to /var/www/html/piviewer\n"
-$target_dir="/var/www/html/piviewer/"
-sudo cp -r PiCamViewer/* $target_dir
+printf "Moving PiViewer to /var/www/html/piviewer ...\n"
+$target_dir="/var/www/html/piviewer"
+if [[ -d "$target_dir" && -d "PiCamViewer" ]]; then
+  sudo cp -r PiCamViewer/* "$target_dir"
+else
+  printf "Directory not found.\n"
+fi
