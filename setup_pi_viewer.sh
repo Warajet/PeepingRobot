c70bd78d480b63aac565da0c727f467e06dbbe29
Nattaphol <rootncdev2015@gmail.com>
2018-12-09 05:06:25 +0700

    Edit setup script

--- setup_pi_viewer.sh
+++ setup_pi_viewer.sh
@@ -7,7 +7,8 @@ git pull
 printf "Moving PiViewer to /var/www/html/piviewer ...\n"
 $target_dir="/var/www/html/piviewer"
 ls $target_dir
-if [[ -d "$target_dir" && -d "PiCamViewer" ]] ; then
+if [ -d "$target_dir" ] && [ -d "PiCamViewer" ]
+then
   sudo cp -r PiCamViewer/* "$target_dir"
 else
   printf "Directory not found.\n"
