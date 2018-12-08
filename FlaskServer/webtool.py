8304bad40308b1013cceb955ac3ebc8c673a044d
Nattaphol <rootncdev2015@gmail.com>
2018-12-09 06:31:17 +0700

    Fix ajax and fix cors

--- FlaskServer/webtool.py
+++ FlaskServer/webtool.py
@@ -1,16 +1,23 @@
 import sys
 
+# pip install Flask
 from flask import Flask, render_template, request, redirect, Response
+# pip install flask-cors
+from flask_cors import CORS, cross_origin
 import random, json
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
+@cross_origin()
 def output():
     # serve index template
     return "Hello World!"
 
 @app.route('/receiver', methods = ['POST'])
+@cross_origin(origin='*',headers=['Content-Type','Authorization'])
 def worker():
     # read json + reply
     data = request.get_json()
