740940ddbacdefef6c37293e0c58acde7f1a487a
Nattaphol <rootncdev2015@gmail.com>
2018-12-09 22:21:49 +0700

    Add while loop to get reqest

--- request_cam_position.py
+++ request_cam_position.py
@@ -1,10 +1,14 @@
 import requests
+import time
 
 url = "http://192.168.0.118:5000/yz_json"
-r = requests.get(url)
 
-print(r.status_code) # 200
-print(r.headers['content-type']) # 'application/json; charset=utf8'
-print(r.encoding) # 'utf-8'
-print(r.text) # u'{"type":"User"...'
-#print(r.json()) #{u'private_gists': 419, u'total_private_repos': 77, ...}
+while(True):
+    r = requests.get(url)
+
+    print(r.status_code) # 200
+    print(r.headers['content-type']) # 'application/json; charset=utf8'
+    print(r.encoding) # 'utf-8'
+    print(r.text) # u'{"type":"User"...'
+    #print(r.json()) #{u'private_gists': 419, u'total_private_repos': 77, ...}
+    time.sleep(0.1)
